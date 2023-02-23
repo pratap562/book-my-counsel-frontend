900608e7e34177b2b41818be6c59124f5b9a1a49
iamgauhar <iamgauhar@yahoo.com>
2023-02-23 18:14:42 +0530

    profile page completed

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -9,9 +9,15 @@ const data = {
   pricing: 1200,
   experience: 4,
   total_jobs: 54,
-  language: ["Hindi", "English", "Spanish"],
+  fluent_language: ["Hindi", "English", "Spanish"],
+  conversational_language: ["German", "Italian", "Korean"],
   skills: ["web development", "java", "html", "CSS"],
   education: [
+    {
+      university: "Research institue India",
+      course: "Bachelor of Development",
+      duration: "2019 - 2022"
+    },
     {
       university: "Research institue India",
       course: "Bachelor of Development",
@@ -22,9 +28,52 @@ const data = {
   long_description: `Hi! My name is Shuska. I'm a native Spanish speaker and fluent in English with a Bachelor's Degree in International Trade.
      I'm a highly results-oriented professional with solid experience in translation and Customer Relation Management (CRM).I take pride in my Upwork profile and for being known as a reliable freelancer; with a pleasant attitude,
       problem-solving skills, and excellent organizational and time management aptitudes. As an experienced customer service advocate for over six years and translator for more than three, I will be happy to help you proofread, localize, translate or be part of your Customer Support Team.Thank you for taking the time to read about me
-    .-Shus`
+    .-Shus`,
+  reviews: [
+    {
+      name: "Mohammad Gauhar",
+      rating: 4,
+      date: "20-20-2022",
+      comment:
+        "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+    },
+    {
+      name: "Mohammad Gauhar",
+      rating: 4,
+      date: "20-20-2022",
+      comment:
+        "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+    },
+    {
+      name: "Mohammad Gauhar",
+      rating: 4,
+      date: "20-20-2022",
+      comment:
+        "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+    },
+    {
+      name: "Mohammad Gauhar",
+      rating: 4,
+      date: "20-20-2022",
+      comment:
+        "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+    },
+    {
+      name: "Mohammad Gauhar",
+      rating: 4,
+      date: "20-20-2022",
+      comment:
+        "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+    },
+    {
+      name: "Mohammad Gauhar",
+      rating: 4,
+      date: "20-20-2022",
+      comment:
+        "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+    }
+  ]
 };
-
 export default function Profile() {
   return (
     <div className="App">
