982ebec41a1eb85ab1b2779c4d4db5c415835b3a
pratap562 <kumpratap562@gmail.com>
2023-02-27 17:15:37 +0530

    admin side removed from code for temprerly creating some problem with deployment

--- pages/admin/AdminHome.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AdminHome = ()=>{
-    return (
-        <h1>Home</h1>
-    )
-}
-
-export {AdminHome}
--- pages/admin/Allapplicants.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Allapplicants = ()=>{
-    return(
-        <h1>All Applicants</h1>
-    )
-}
-
-export {Allapplicants}
